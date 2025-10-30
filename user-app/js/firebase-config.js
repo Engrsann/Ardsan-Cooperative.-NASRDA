fc296721f0b8250bc78bba5763259ffddd7ce7ec
Engrsann <82672393+Engrsann@users.noreply.github.com>
2025-10-30 07:25:28 -0700

    Update firebase-config.js

--- user-app/js/firebase-config.js
+++ user-app/js/firebase-config.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
+  databaseURL: "https://console.firebase.google.com/project/ardsan-cooperative/database/ardsan-cooperative-default-rtdb/data/~2F",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_SENDER_ID",
