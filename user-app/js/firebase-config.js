bebe8f3ebcff0f71cc8d2dd61a5debe9f7260f52
Engrsann <82672393+Engrsann@users.noreply.github.com>
2025-10-30 08:15:07 -0700

    Update Firebase configuration with project details

--- user-app/js/firebase-config.js
+++ user-app/js/firebase-config.js
@@ -2,10 +2,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDTohhMUjXKOMxNHDXBN4c0vx9wp5rMMgs",
   authDomain: "YOUR_AUTH_DOMAIN",
   databaseURL: "https://console.firebase.google.com/project/ardsan-cooperative/database/ardsan-cooperative-default-rtdb/data/~2F",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "ardsan-cooperative",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  appId: "1:1014264430413:android:321a2847f4e8b2fcdf8104"
 };
 
 firebase.initializeApp(firebaseConfig);
